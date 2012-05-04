06163cb44507e78803a38e4ca1687fcff6fe53f5
Wesley Alcoforado <wesleyjefferson@gmail.com>
2012-05-04 16:59:59 -0300

    solucao do problema 14

---
+++ 0014/CollatzProblem.cs
@@ -0,0 +1,41 @@
+using System;
+
+class CollatzProblem
+{
+	static void Main(string[] args)
+	{
+		int menorNumero = 1, maiorCadeia = 1;
+
+		for (int inicioCadeia = 2; inicioCadeia < 1000000; inicioCadeia++)
+		{
+			int tamanhoCadeia = TamanhoCadeia(inicioCadeia);
+			if (tamanhoCadeia > maiorCadeia) 
+			{
+				menorNumero = inicioCadeia;
+				maiorCadeia = tamanhoCadeia;
+			}
+		}
+
+		Console.WriteLine("O menor numero abaixo de 1 milhao que produz a maior cadeia eh: {0}", menorNumero);
+	}
+
+	private static int TamanhoCadeia(int numero) 
+	{
+		int tamanhoCadeia = 1;
+
+		for (ulong i = (ulong)numero; i > 1; i = ProximoNumero(i))
+		{
+			tamanhoCadeia++;
+		}
+
+		return tamanhoCadeia;
+	}
+
+	private static ulong ProximoNumero(ulong n)
+	{
+		if (n % 2 == 0)
+			return n / 2;
+
+		return 3 * n + 1;
+	}
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-project_euler
+Project Euler
 =============
 
-Project Euler
+Soluções dos exercícios do site http://projecteuler.net
